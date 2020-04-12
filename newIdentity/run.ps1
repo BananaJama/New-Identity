60bf54a0ebb703131a77fe4183e0bcaabeb94f20
Jeremy Brown <jeremy.brown@nccourts.org>
2020-04-11 20:36:15 -0400

    Add UserCount to support multiple users returned

--- newIdentity/run.ps1
+++ newIdentity/run.ps1
@@ -9,21 +9,23 @@ param($Request, $TriggerMetadata, $GivenNames, $Surnames, $CityData, $Depts)
 Write-Host "PowerShell HTTP trigger function processed a request."
 
 # Interact with query parameters or the body of the request.
+$UserCount = $Request.Query.UserCount
 $City = $Request.Query.City
 $Dept = $Request.Query.Dept
 $State = $Request.Query.State
 $GivenNames = $GivenNames -split ','
 $Surnames = $Surnames -split ','
 $Departments = $Depts -split ','
+$NewRandomCityRequired = $false
+$NewRandomDeptRequired = $false
 
-$GivenName = Get-Random $GivenNames
-$GivenName = $GivenName.Substring(0,1).ToUpper() + $GivenName.Substring(1)
-
-$Surname = Get-Random $Surnames
-$Surname = $Surname.Substring(0,1).ToUpper() + $Surname.Substring(1)
+if (-not ($UserCount)) {
+    $UserCount = 1
+}
 
 if ($Departments -notcontains $Dept) {
     $Dept = Get-Random $Departments
+    $NewRandomDeptRequired = $true
 }
 
 if ($City -and $State) {
@@ -32,21 +34,42 @@ if ($City -and $State) {
 
 if (-not ($City.Count -gt 1)) {
     $City = Get-Random $CityData
+    $NewRandomCityRequired = $true
 }
 
-$Identity = [psobject]@{
-    GivenName = $GivenName
-    Surname = $Surname
-    City = $City.city
-    StateId = $City.state_id
-    StateName = $City.state_name
-    TimeZone = $City.timezone
-    ZipCodes = $City.zips
-    Dept = $Dept
+$Identities = [System.Collections.Generic.List[psobject]]::new()
+
+for ($i=0; $i -lt $UserCount; $i++) {
+    $GivenName = Get-Random $GivenNames
+    $GivenName = $GivenName.Substring(0,1).ToUpper() + $GivenName.Substring(1)
+
+    $Surname = Get-Random $Surnames
+    $Surname = $Surname.Substring(0,1).ToUpper() + $Surname.Substring(1)
+
+    if ($NewRandomCityRequired) {
+        $City = Get-Random $CityData
+    }
+
+    if ($NewRandomDeptRequired) {
+        $Dept = Get-Random $Departments
+    }
+
+    $Identity = [psobject]@{
+        GivenName = $GivenName
+        Surname = $Surname
+        City = $City.city
+        StateId = $City.state_id
+        StateName = $City.state_name
+        TimeZone = $City.timezone
+        ZipCodes = $City.zips
+        Dept = $Dept
+    }
+
+    $Identities.Add($Identity)
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    Body = $Identity
+    Body = $Identities
 })
 
